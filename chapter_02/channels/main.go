30fea0fcf6fd65a1d1f973991b698b8cb4bfe8b6
Asanbek09 <samudinua@gmail.com>
2026-03-16 16:28:27 +0600

    Примеры по второй главе - channels, generics и panics

---
+++ chapter_02/channels/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func sendMessage(message string, ch chan string) {
+	ch <- message
+}
+
+func main() {
+	ch := make(chan string, 6)
+	go sendMessage("Hello", ch)
+	go sendMessage("World", ch)
+
+	fmt.Println(<- ch)
+	fmt.Println(<- ch)
+}

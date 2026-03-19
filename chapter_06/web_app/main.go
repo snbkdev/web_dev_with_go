1387ba1283e92a188f8fe5ffeaba35e95fef246a
Asanbek09 <samudinua@gmail.com>
2026-03-19 17:07:23 +0600

    Начало реализации веб проекта

---
+++ chapter_06/web_app/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"web_app/config"
+)
+
+func main() {
+	log.Println("Starting Runners App")
+	log.Println("Initializing configuration")
+	config := config.InitConfig("runners")
+	log.Println("Initializing database")
+	dbHandler := server.InitDatabase(config)
+	log.Println("Initializing HTTP Server")
+	httpServer := server.InitHttpServer(config, dbHandler)
+	httpServer.Start()
+}

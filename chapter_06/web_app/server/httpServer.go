275f24b5c0ef9336e676cdd2cb6e4e75258b616e
Asanbek09 <samudinua@gmail.com>
2026-03-23 10:12:30 +0600

    runnersController.go - реализация

--- chapter_06/web_app/server/httpServer.go
+++ chapter_06/web_app/server/httpServer.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"database/sql"
+	"log"
 
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
@@ -37,5 +38,8 @@ func InitHttpServer(config *viper.Viper, dbHandler *sql.DB) HttpServer {
 }
 
 func (hs HttpServer) Start() {
-	//
+	err := hs.router.Run(hs.config.GetString("http.server_address"))
+	if err != nil {
+		log.Fatalf("Error while starting HTTP server: %v", err)
+	}
 }
